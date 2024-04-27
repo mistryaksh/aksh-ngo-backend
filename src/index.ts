88c252804c8bfd6116c17ed5541349b4ba33b865
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-04-27 18:54:33 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import { registerRoutesV1 } from "./api";
 
 const corsOptions: CorsOptions = {
   credentials: true,
-  origin: ["https://ngo-demo.aksh.ltd", "https://ngo-admin-demo.aksh.ltd/"],
+  origin: "*",
 };
 
 dotenv.config();
